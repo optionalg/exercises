bee5b5cf89380eb6f791e3f0ff509b26af10c86d
Humza Yaqoob <contact@secondplanetanimation.com>
2014-04-10 22:10:34 -0400

    exercism ruby solve gigasecond

--- exercism/ruby/gigasecond/gigasecond_test.rb
+++ exercism/ruby/gigasecond/gigasecond_test.rb
@@ -11,13 +11,11 @@ class GigasecondTest < MiniTest::Unit::TestCase
   end
 
   def test_2
-    skip
     gs = Gigasecond.new(Date.new(1977, 6, 13))
     assert_equal Date.new(2009, 2, 19), gs.date
   end
 
   def test_3
-    skip
     gs = Gigasecond.new(Date.new(1959, 7, 19))
     assert_equal Date.new(1991, 3, 27), gs.date
   end
