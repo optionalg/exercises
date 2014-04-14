d440abc17f02e7a03e234ccf56e5494020ae730d
Humza Yaqoob <contact@secondplanetanimation.com>
2014-04-14 09:16:16 -0400

    c prog lang: ex 1-12

---
+++ TheCProgrammingLanguage/Ch1/1-12/wordprint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define IN  1
+#define OUT 0
+
+main()
+{
+
+  int c, state;
+
+  state = OUT;
+
+  while ((c = getchar()) != EOF) {
+    if (c != ' ' && c != '\n' && c != '\t') {
+      putchar(c);
+      state = OUT;
+    }
+    else if (state == OUT) {
+      putchar('\n');
+      state = IN;
+    }
+  }
+
+}
