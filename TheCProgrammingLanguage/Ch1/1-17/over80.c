3cda81da2d585205315ec7a6cbb2ecdfb0f3ac92
Humza Yaqoob <contact@secondplanetanimation.com>
2014-04-16 13:38:31 -0400

    c prog lang: ex 1-17

---
+++ TheCProgrammingLanguage/Ch1/1-17/over80.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+// a getline function is already present in stdio
+int _getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+main()
+{
+
+  int len;
+  int cur;
+  char line[MAXLINE];
+  char lntxt[MAXLINE];
+
+  cur = 0;
+
+  while ((len = _getline(line, MAXLINE)) > 0) {
+    cur = len;
+    copy(lntxt, line);
+    if (cur > 80) {
+      printf("%s", lntxt);
+    }
+  }
+
+  return 0;
+
+}
+
+int _getline(char s[], int lim) {
+
+  int c, i;
+
+  for (i=0; i < (lim - 1) && (c = getchar()) != EOF && c != '\n'; ++i) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i] = c;
+    ++i;
+  }
+
+  s[i] = '\0';
+  return i;
+
+}
+
+void copy(char to[], char from[]) {
+
+  int i;
+
+  i = 0;
+
+  while ((to[i] = from[i]) != '\0') {
+    ++i;
+  }
+
+}
